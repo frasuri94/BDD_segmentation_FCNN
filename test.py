09a8fe77c59bd0695ee185bcc92016fb6c3ffeb6
frasuri94 <fra.suri94@gmail.com>
2018-10-06 18:11:53 +0200

    loading

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from keras.layers import *
 from keras.activations import softmax
 import tensorflow as tf
 
-
+########
 tensor = tf.convert_to_tensor([
           [2.0, 1.0, 0.1],
           [0.1, 1.0, 2.0],
